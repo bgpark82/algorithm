0ad47313dc98a7b10c353ca084588b82c7198915
bgpark <bgpark@citus.co.kr>
2020-08-27 18:30:54 +0900

    B2529

---
+++ src/main/java/brute_force/recursion/B2529.java
@@ -0,0 +1,9 @@
+package brute_force.recursion;
+
+public class B2529 {
+
+    public static void main(String[] args) {
+        // 1. bfs로 순열 구하기
+        // 2. 순열에서 오름차순, 내림차순으로 최대값, 최소값 구한다
+    }
+}
