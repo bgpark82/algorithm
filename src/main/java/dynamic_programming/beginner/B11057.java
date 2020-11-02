16cfe176f556254c1730c1c810d9e0264b78480f
bgpark <bgpark82@gmail.com>
2020-11-03 01:20:04 +0900

    B11057

---
+++ src/main/java/dynamic_programming/beginner/B11057.java
@@ -0,0 +1,30 @@
+package dynamic_programming.beginner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B11057 {
+
+    static private int[][] answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        answer = new int[size + 1][10];
+        Arrays.fill(answer[1], 1);
+
+        for (int i = 2; i <= size; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                        answer[i][j] += answer[i-1][k];
+                        answer[i][j] %= 10007;
+
+                }
+            }
+        }
+
+        int sum = Arrays.stream(answer[size]).sum();
+        System.out.println(sum%10007);
+    }
+}
