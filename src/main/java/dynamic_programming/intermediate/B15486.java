a9d8a7911199e4d3aec1d3b6d409742781c01e46
bgpark <bgpark82@gmail.com>
2020-11-06 02:02:45 +0900

    B15486

--- src/main/java/dynamic_programming/intermediate/B15486.java
+++ src/main/java/dynamic_programming/intermediate/B15486.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 
 public class B15486 {
 
+    private static Schedule[] schedules;
+    private static int[] answer;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int size = sc.nextInt();
-        Schedule[] schedules = new Schedule[size];
         sc.nextLine();
+
+        schedules = new Schedule[size];
+        answer = new int[size];
 
         for (int i = 0; i < size; i++) {
             String[] s = sc.nextLine().split(" ");
             schedules[i] = new Schedule(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
         }
 
+        for (int i = 0; i < size; i++) {
+            int day = schedules[i].day;
+            int pay = schedules[i].pay;
 
+            answer[i + day] = Math.max(answer[i] + pay, answer[i + day]);
+            answer[i + 1] = Math.max(answer[i], answer[i+1]);
+        }
+
+        System.out.println(answer[size-1]);
     }
+
 
     static class Schedule {
         int day;
